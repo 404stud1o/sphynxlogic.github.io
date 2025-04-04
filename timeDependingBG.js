f11c527a60a9604f60e0ff39545fc3caf4f53ca9
Henry Li <73312050+404stud1o@users.noreply.github.com>
2025-04-04 03:56:45 +0100

    minor minor sub changes

--- timeDependingBG.js
+++ timeDependingBG.js
@@ -3,9 +3,11 @@ function setBackgroundImageByTime() {
     const hour = now.getHours();
     let imageUrl;
 
+    // 0600 - 1800, Day
     if (hour >= 6 && hour < 18) {
         imageUrl = 'url("/Assets/SHILLING-day-bg.jpg")'; 
     } 
+    // 1800 - 0600 and all other occasions, Night
     else if (hour >= 18 && hour < 23) {
         imageUrl = 'url("/Assets/SHILLING-night-bg.jpg")'; 
     } 
@@ -16,13 +18,13 @@ function setBackgroundImageByTime() {
         imageUrl = 'url("/Assets/SHILLING-night-bg.jpg")'; 
     }
 
-    // Apply the background image to the desired element
-    const element = document.body; // Or any other element
+    // Apply the background image to the header background
+    const element = document.body;
     element.style.backgroundImage = imageUrl;
 }
 
 // Call the function when the page loads
 window.onload = setBackgroundImageByTime;
 
-// Optionally, update the background image every sec
+// Update the background image every second
 setInterval(setBackgroundImageByTime, 6000);
